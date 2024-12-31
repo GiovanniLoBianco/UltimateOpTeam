FIX: objective numerical error

src/ultimate_opteam/optim/milp_model.py
@@ -327,6 +327,10 @@ class UT_MILP_Model:
             <= 10
         )
 
+    def _get_current_objective(self) -> int:
+        """Get current objective value."""
+        return int(np.round(self.solver.Objective().Value()))
+
     def solve(self) -> list[Team]:
         """Find all optimal solutions and return them as teams."""
         solutions: list[Team] = []
@@ -340,16 +344,16 @@ class UT_MILP_Model:
             if status in [pywraplp.Solver.OPTIMAL, pywraplp.Solver.FEASIBLE]:
                 # initialize best_obj
                 logger.info(
-                    f"Solution found! Objective value: {self.solver.Objective().Value()}"
+                    f"Solution found! Objective value: {self._get_current_objective()}"
                 )
                 if not solutions:
-                    best_obj = self.solver.Objective().Value()
+                    best_obj = self._get_current_objective()
 
                 # extract team from current solution
                 team = self._extract_team_from_solution()
 
                 # if solution is worse than best_obj then break
-                if self.solver.Objective().Value() < best_obj:
+                if self._get_current_objective() < best_obj:
                     logger.info("Solution found is worse than previous solution. Stop.")
                     break
 